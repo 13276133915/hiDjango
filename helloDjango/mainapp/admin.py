499be4c783cfc0130ec71b0d59dc23ad33fad113
lihu <lihu>
2021-05-19 21:55:57 +0800

    StoreEntity

--- helloDjango/mainapp/admin.py
+++ helloDjango/mainapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from mainapp.models import UserEntity, CateTypeEntity, FuritRntity
+from mainapp.models import UserEntity, CateTypeEntity, FuritRntity, StoreEntity
 
 # Register your models here.
 
@@ -18,8 +18,14 @@ class CateTypeAdmin(admin.ModelAdmin):
 class FruitAdmin(admin.ModelAdmin):
     list_display = ('name', 'source', 'price', 'category')
 
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('id_', 'name', 'adress', 'city', 'store_type')
+    # 指定表单修改的字段
+    fields = ('name', 'city', 'adress', 'store_type')
+
 
 # 将模型增加到站点中
 admin.site.register(UserEntity, Useradmin)
 admin.site.register(CateTypeEntity, CateTypeAdmin)
 admin.site.register(FuritRntity, FruitAdmin)
+admin.site.register(StoreEntity, StoreAdmin)
--- helloDjango/mainapp/models.py
+++ helloDjango/mainapp/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
@@ -47,3 +49,52 @@ class FuritRntity(models.Model):
         db_table = 'app_fruit'
         verbose_name = '水果表'
         verbose_name_plural = verbose_name
+
+
+class StoreEntity(models.Model):
+    # 可以显示的方式声明主键
+    id = models.UUIDField(primary_key=True,
+                          verbose_name="商店编号")
+
+    name = models.CharField(max_length=50,
+                            verbose_name="店名",
+                            unique=True) # unique唯一约束
+    # 表中对应的字段 type_
+    store_type = models.IntegerField(choices=((0,"自营"),(1,"第三方")),
+                                     verbose_name="店铺类型",
+                                     db_column="type_")
+
+    adress = models.CharField(max_length=100,
+                              verbose_name="地址")
+    # 城市可能在搜索框中使用
+    city = models.CharField(max_length=50,
+                            verbose_name="城市",
+                            db_index=True) # db_index 索引
+    create_time = models.DateTimeField(verbose_name="创建时间",
+                                   auto_now_add=True,
+                                       null=True) # 创建当前时间
+    last_time = models.DateField(verbose_name="最后变更时间",
+                                 auto_now=True,
+                                 null=True) # 每次更新都会增加当前时间
+
+    def __str__(self):
+        return self.name
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        # 调用模型保存方法的时候 被调用
+        # 判断是否为新增的
+        if not self.id:
+            self.id = uuid.uuid4().hex
+        super().save()
+
+
+    @property
+    def id_(self):
+        return str(self.id).replace('-','')
+
+    class Meta:
+        db_table = "app_store"
+        verbose_name = "水果店铺"
+        verbose_name_plural = verbose_name
+        unique_together = (('name','city'),) #组合来辨识重复值
